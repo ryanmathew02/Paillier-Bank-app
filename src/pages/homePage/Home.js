39882786efd9db26bab3bc658a4f606a1a23052a
ryanmathew02 <ryanmathew.career@gmail.com>
2024-03-31 21:13:13 +0530

    check balance

--- src/pages/homePage/Home.js
+++ src/pages/homePage/Home.js
@@ -1,6 +1,21 @@
+import axios from 'axios'
 import React from 'react'
+import { useState } from 'react'
 
 const Home = ({ userDetails }) => {
+    const [balancepassword, setbalancepassword] = useState("")
+    const [balance, setbalance] = useState("")
+
+    async function UserBalance() {
+        axios.get('http://localhost:5000/transfer/balance', { withCredentials: true })
+            .then(result => {
+                console.log(result);
+                setbalance(result.data.balance);
+            })
+            .catch(err => {
+                console.log(err);
+            })
+    }
     return (
         <div className='Home-box'>
             <div className="accountdetail-head">ACCOUNT DETAILS:</div>
@@ -16,6 +31,13 @@ const Home = ({ userDetails }) => {
                 <div className="accountdetails-content">
                     <p>Balance:</p>
                     <p></p>
+                </div>
+                <br />
+                <div className="accountdetails-content">
+                    <p>Check Balance:</p>
+                    <input value={balancepassword} placeholder='Transaction Password' onChange={(e) => setbalancepassword(e.target.value)}></input>
+                    <button onClick={UserBalance}>Check</button>
+                    <p>{balance}</p>
                 </div>
             </div>
         </div>
